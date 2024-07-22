f6bc6b0428c4bfd3e32605bed47818a377ca9e28
SAPTAK SARKAR <115360675+Saptak-2001@users.noreply.github.com>
2024-07-22 09:20:22 +0530

    Updated ForgotPassword.js

--- frontend/src/pages/ForgotPassword.js
+++ frontend/src/pages/ForgotPassword.js
@@ -13,7 +13,7 @@ export default function ForgotPassword() {
     const handleSubmit = async(e) => {
         e.preventDefault();
         try {
-            const res = await axios.post(`${window.location.origin}/api/auth/forgot-password`, {
+            const res = await axios.post('http://localhost:8000/api/auth/forgot-password', {
                 email,
                 newPassword,
                 userName
